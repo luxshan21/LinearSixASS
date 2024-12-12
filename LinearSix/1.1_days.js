36b98a931d189084797206f0022e5f54ba4293c0
Luxshan <81348451+luxshan21@users.noreply.github.com>
2024-12-12 12:59:35 +0530

    updated answer

--- LinearSix/1.1_days.js
+++ LinearSix/1.1_days.js
@@ -1,9 +1,14 @@
 Date.prototype.daysTo = function(targetDate) {
+
+    if (isNaN(this.getTime()) || isNaN(targetDate.getTime())) {
+        throw new Error("date is invalid."); // validate the d1, d2 input date
+    }
+
     const timeDifference = targetDate.getTime() - this.getTime(); // convert date to milisecond timestamp
     const countDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     return countDays;
 };
 
-const d1 = new Date('2024-12-10');
+const d1 = new Date('2024-12-12');
 const d2 = new Date('2024-12-15');
 console.log(d1.daysTo(d2)); 
